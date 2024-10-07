cfef580e7655d533d759f34d0e6f8f4424bd322f
punkmohamed <punkmohamed22@gmail.com>
2024-10-07 23:57:19 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Chip from "./components/Chip"
+import Features from "./components/Features"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import HighLights from "./components/HighLights"
+import Model from "./components/Model"
+import Navbar from "./components/Navbar"
+
+
+function App() {
+
+  return (
+    <>
+      <main className="bg-black">
+        <Navbar />
+        <Hero />
+        <HighLights />
+        <Model />
+        <Features />
+        <Chip />
+        <Footer />
+      </main>
+    </>
+  )
+}
+
+export default App
